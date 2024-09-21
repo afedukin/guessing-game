f237629f3d4929a1dfc4796915ae2a92d210d3a8
afedukin <fedukin.aa@yandex.ru>
2024-09-21 18:09:03 +0400

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min
+        this.max
+        this.center
+    }
+    
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
-
+        this.center = Math.ceil((this.min + this.max) / 2); 
+        return this.center
     }
 
     lower() {
+        this.max = this.center
 
     }
 
     greater() {
-
+        this.min = this.center
     }
 }
 
